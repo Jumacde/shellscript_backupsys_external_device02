f30f8e91a420a83e6dd44a957bdfadc61ba0f276
Jumacde <y.takamura0407.pokemon.go@gmail.com>
2026-07-09 22:56:53 +0200

    success test

--- scripts/device_manager.sh
+++ scripts/device_manager.sh
@@ -16,41 +16,62 @@ target_dir2="${device_dir2}"
 mount_result=""
 
 check_mount() {
-	# if both devices are not mounted.
-	if [ ! -d "$(dirname "$target_dir1")" ] && [ ! -d "$(dirname "$target_dir2")" ]; then
-		log_message  "FAILD" "both devices are not mounted."
-		echo "FAILED"
-	# if only a devices is mounted.
-	else if [ -d "$(dirname "$target_dir1")" ] && [ ! -d "$(dirname "$target_dir2")" ]; then
-                log_message  "WARN" "only ${target_dir1} is mounted."
-                echo "WARN1"
-	else if [ ! -d "$(dirname "$target_dir1")" ] && [ -d "$(dirname "$target_dir2")" ]; then
-                log_message  "WARN" "only ${target_dir1} is mounted."
-                echo "WARN2"
-	# if both devices are mounted-
-	else 
-		log_message "SUCCESS" "${target_dir1} and ${target_dir2} are mounted."
-		echo "SUCCESS"
-	fi
-	mkdir -p "$(dirname "$target_dir1")"
-        mkdir -p "$(dirname "$target_dir2")"
+	local mount1=0
+	local mount2=0
+	# chck mount status both devices
+	[[ -d "$(dirname "$target_dir1")" ]] && mount1=1
+        [[ -d "$(dirname "$target_dir2")" ]] && mount2=1
+
+	case "${mount1}${mount2}" in
+		# if both devices are mounted.
+		"11")	
+			mkdir -p "$(dirname "$target_dir1")"
+        		mkdir -p "$(dirname "$target_dir2")"
+			log_message "SUCCESS" "${target_dir1} and ${target_dir2} are mounted."
+                        echo "SUCCESS"
+                        ;;
+		# if only device1 is mounted
+		"10")	
+			mkdir -p "$(dirname "$target_dir1")"
+			log_message "WARN" "only ${target_dir1} is mounted."
+                        echo "WARN1"
+                        ;;
+		# if only device2 is mounted
+		"01")
+			mkdir -p "$(dirname "$target_dir2")"
+			log_message "WARN" "only ${target_dir2} is mounted."
+                        echo "WARN2"
+                        ;;
+		# if both devices are not mounted
+		"00") 
+			log_message "FAILD" "both devices are not mounted."
+                        echo "FAILED"
+                        ;;
+	esac
 }
 
 start_backup() {
 	mount_result=$(check_mount)
-if [ "$mount_result" = "SUCCESS" ]; then
-	rsync -av --delete "$src_dir" "$target_dir1" "$target_dir2">> "$log_dir" 2>&1
-	log_message "SUCCESS" "backup process to ${target_dir1} and ${target_dir2}  is successfully finished."
-	exit 0
-else if [ "$mount_result" = "WARN1" ]; then
-        mkdir -p "$(dirname "$target_dir1")"
-        rsync -av --delete "$src_dir" "$target_dir1" >> "$log_dir" 2>&1
-        log_message "SUCCESS" "backup process to ${target_dir1} is successfully finished."
-        exit 0
-else if [ "$mount_result" = "WARN2" ]; then
-        mkdir -p "$(dirname "$target_dir2")"
-        rsync -av --delete "$src_dir"  "$target_dir2">> "$log_dir" 2>&1
-        log_message "SUCCESS" "backup process to ${target_dir2}  is successfully finished."
-        exit 0
-fi
+	case "$mount_result" in
+		"SUCCESS")
+                        rsync -av --delete "$src_dir" "$target_dir1" >> "$log_dir" 2>&1
+                        rsync -av --delete "$src_dir" "$target_dir2" >> "$log_dir" 2>&1
+                        log_message "SUCCESS" "backup process to both devices is successfully finished."
+                        exit 0
+                        ;;
+		"WARN1")
+			rsync -av --delete "$src_dir" "$target_dir1" >> "$log_dir" 2>&1
+                        log_message "SUCCESS" "backup process to ${target_dir1} is successfully finished."
+                        exit 0
+                        ;;
+		"WARN2")
+			mkdir -p "$target_dir2"
+                        rsync -av --delete "$src_dir" "$target_dir2" >> "$log_dir" 2>&1
+                        log_message "SUCCESS" "backup process to ${target_dir2} is successfully finished."
+                        exit 0
+                        ;;
+		"FAILED")
+			exit 1
+			;;
+	esac
 }
